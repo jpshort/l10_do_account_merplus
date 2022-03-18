ec7e069ebfe5f07f9df5269dfa7bb9d8645ffbfe
David ROCA <davidjromeroc@gmail.com>
2022-03-18 12:31:27 -0500

    [FIX] DJRC Corrigio dependencia por cambio de nombre

--- dgii_reports/__manifest__.py
+++ dgii_reports/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '15.0.1.1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'l10n_do_accounting_plus'],
+    'depends': ['base', 'l10n_do_accounting'],
 
     # always loaded
     'data': [
